971fdb18571a0381f430df285c09cc7b20b509c3
chamilton15 <cphamilton1000@yahoo.com>
2024-02-15 18:28:45 -0500

    add git ignore

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import NewExpense from './components/NewExpense/NewExpense';
 const DUMMY_EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Basketball",
+    amount: 50,
+    date: new Date(2023, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "Spikeball", amount: 55, date: new Date(2023, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: "Football",
+    amount: 20,
+    date: new Date(2023, 2, 28),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: "Ball Pump",
+    amount: 11.50,
+    date: new Date(2024, 1, 12),
   },
 ];
 
